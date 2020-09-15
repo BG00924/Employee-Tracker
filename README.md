7698391fb79d4f2e8e842784291083efc351ce51
BG00924 <BG00924@users.noreply.github.com>
2020-09-15 17:03:43 -0500

    mvp

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Employee-Tracker
+
+## Description
+
+* Employee Tracker is a utilitarian that provides employers a simple and effective means to organize their employees.
+
+## Usage
+
+* Simply clone the repository and initiate from the command line
+
+## Credits:
+
+* Node Express
+* MySql
+* Various documentation accessed via google
+
+## Questions:
+  
+For additional information visit my GitHub page:
+* [GitHub Profile](https://github.com/bg00924)
+
+For questions please send me an email at:
+* bg00924@gmail.com
+
+## Preview
+
+![Overview of Employee Tracker](./assets/employee_tracker_example.JPG)
--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ class DB {
         this.connection = connection;
     }
 
-    findAllEmployees() {
+    viewEmployees() {
         return this.connection.promise().query(
             `SELECT employee.id, employee.first_name,
              employee.last_name, job.title, departments.name
@@ -18,16 +18,72 @@ class DB {
         )
     }
 
-    updateEmployeeRole(employee_id, job_id) {
+    newEmployee(employee) {
         return this.connection.promise().query(
-            `UPDATE employee SET job_id = ? WHERE id = ?`, [job_id, employee_id]
+            `INSERT INTO employee SET ?`, employee
         )
+    }
+
+    deleteEmployee(employee_id) {
+        return this.connection.promise().query(
+            `DELETE FROM employee WHERE id = ?`, employee_id
+        )
+    }
+
+    updateEmployeeJob(employee_id, job_id) {
+        return this.connection.promise().query(
+            `UPDATE employee SET job_id = ? WHERE id = ?`, 
+            [job_id, employee_id]
+        )
+    }
+
+    updateEmployeeManager(employee_id, manager_id) {
+        return this.connection.promise().query(
+            `UPDATE employee SET manager_id = ? WHERE id = ?`, 
+            [manager_id, employee_id]
+        )
+    }
+
+    viewManagers(employee_id) {
+        return this.connection.promise().querry(
+            `SELECT id, first_name, last_name FROM employee WHERE id != ?`, employee_id
+        )
+    }
+
+    viewDepartments() {
+        return this.connection.promise().query(
+            `SELECT departments.id, departments.name FROM departments`)
     }
 
     createDepartment(department) {
         return this.connection.promise().query(
             `INSERT INTO departments SET ?`, department
         )
+    }
+
+    deleteDepartment(departments_id) {
+        return this.connection.promise().query(
+            `DELETE from departments WHERE id = ?`, departments_id
+        )
+    }
+
+    viewJobs() {
+        return this.connection.promise().query(
+            `SELECT job.id, job.title, departments.name AS departments, 
+            job.salary FROM job LEFT JOIN departments on job.departments_id
+            = departments.id;`
+        )
+    }
+
+    newJob(job) {
+        return this.connection.promise().query(
+            `INSERT INTO job SET ?`, job
+        )
+    }
+
+    deleteJob(job_id) {
+        return this.connection.promise().query(`
+            DELETE FROM job ? WHERE id = ?`, job_id)
     }
 }
 
